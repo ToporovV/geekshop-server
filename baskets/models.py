ec79786e71fd954a3d66983940cc2de26ae602b6
toporovv <top.vetal@yandex.ru>
2021-06-19 01:22:10 +0300

    added admins app and templates

--- baskets/models.py
+++ baskets/models.py
@@ -15,13 +15,12 @@ class Basket(models.Model):
     def sum(self):
         return self.quantity * self.product.price
 
-    def basket_objects(self):
+    @property
+    def baskets(self):
         return Basket.objects.filter(user=self.user)
 
     def total_quantity(self):
-        self.basket_objects()
-        return sum(basket.quantity for basket in self.basket_objects())
+        return sum(basket.quantity for basket in self.baskets)
 
     def total_sum(self):
-        self.basket_objects()
-        return sum(basket.sum() for basket in self.basket_objects())
+        return sum(basket.sum() for basket in self.baskets)
