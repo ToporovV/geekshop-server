0b2bb3d80f62b41746d9c9fc733ee2c48916fe3c
toporovv <top.vetal@yandex.ru>
2021-06-22 07:11:01 +0300

    added class UserCreateView to admins view

--- admins/views.py
+++ admins/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from users.models import User
 from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -23,17 +24,23 @@ class UserListView(ListView):
     template_name = 'admins/admin-users-read.html'
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegisterForm()
-    context = {'title': 'GeekShop - Админ | Регистрация', 'form': form}
-    return render(request, 'admins/admin-users-create.html', context)
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminRegisterForm()
+#     context = {'title': 'GeekShop - Админ | Регистрация', 'form': form}
+#     return render(request, 'admins/admin-users-create.html', context)
+
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegisterForm
+    success_url = reverse_lazy('admins:admin_users')
 
 
 @user_passes_test(lambda u: u.is_superuser)
