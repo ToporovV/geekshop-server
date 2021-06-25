4f841b854a904565300889712e4a0c68af6137ea
toporovv <top.vetal@yandex.ru>
2021-06-25 06:46:34 +0300

    update products.html

--- products/views.py
+++ products/views.py
@@ -12,35 +12,35 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None, page=1):
-    context = {'title': 'GeekShop-Каталог', 'categories': ProductCategory.objects.all()}
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-
-    paginator = Paginator(products, 3)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-    context['products'] = products_paginator
-    return render(request, 'products/products.html', context)
-
-
-# class ProductsListView(ListView):
-#     model = Product
-#     template_name = 'products/products.html'
-#     paginate_by = 3
+# def products(request, category_id=None, page=1):
+#     context = {'title': 'GeekShop-Каталог', 'categories': ProductCategory.objects.all()}
+#     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 #
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super(ProductsListView, self).get_context_data(**kwargs)
-#         context['title'] = 'GeekShop - Административная панель'
-#         context['categories'] = ProductCategory.objects.all()
-#         return context
-#
-#     def get_queryset(self):
-#         if 'category_id' in self.kwargs:
-#             self.category_id = get_object_or_404(ProductCategory, id=self.kwargs['category_id'])
-#             return Product.objects.filter(category_id=self.category_id)
-#         else:
-#             return Product.objects.all()
+#     paginator = Paginator(products, 3)
+#     try:
+#         products_paginator = paginator.page(page)
+#     except PageNotAnInteger:
+#         products_paginator = paginator.page(1)
+#     except EmptyPage:
+#         products_paginator = paginator.page(paginator.num_pages)
+#     context['products'] = products_paginator
+#     return render(request, 'products/products.html', context)
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Административная панель'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+    def get_queryset(self):
+        if 'category_id' in self.kwargs:
+            self.category_id = get_object_or_404(ProductCategory, id=self.kwargs['category_id'])
+            return Product.objects.filter(category_id=self.category_id)
+        else:
+            return Product.objects.all()
